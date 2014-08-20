418fafcf59875891a2e0926e2e0f0bf0cd7214e1
Bruno Gasparetto <brunogasparetto@gmail.com>
2014-08-19 22:10:13 -0400

    Adaptado para boilerplate. Melhoria do README e Exemplo

--- jquery.fontResizer.js
+++ jquery.fontResizer.js
@@ -1,154 +1,137 @@
 /*! 
- * jquery.fontResizer - v 1.0
+ * jquery.fontResizer - v 1.4
  * Copyright (c) 2014 Eder Lima - http://ederlima.com.br
  * License Attribution-ShareAlike 2.5 Generic (CC BY-SA 2.5) http://creativecommons.org/licenses/by-sa/2.5/
+ *
+ * Contributor: Bruno Gasparetto <brunogasparetto@gmail.com>
  */
-// Created: 2014-08-15 
-// Updated: 2014-08-15 
-// REQUIRES: jquery 1.7.x
 
 /*
  * Options
- * @readarea The text container (default this)
- * @incsteps Ammount of resizes (default 5)
- * @incsize Size to add or remove from each element (default 2) (pixel)
- * @smallerbutton jQuery element to decrease the size
- * @biggerbutton jQuery element to increase the size
- * @elements An array of elements (strings, eg: 'li', 'p') to resize within the container
- * @callback A callback function to call on each resize (good for scrolling plugins or functions)
+ *
+ * steps - Ammount of resizes (default 5)
+ * sizeIncrement - Size to add or remove from each element (default 2)
+ * smallerButton - Element to decrease the size (a valid jQuery selector)
+ * biggerButton - Element to increase the size (a valid jQuery selector)
+ * elements - An array of elements (strings, eg: 'li', 'p') to resize within the container
+ * onIncrease - A callback function to call after increase the font size
+ * onDecrease - A callback function to call after decrease the font size
+ *
+ * The callbacks receive the event, current step, max steps, the buttons (smallerButton and biggerButton) and the container.
+ * The context in the callbacks is the element trigger the font resize.
 */
+(function($) {
 
-(function($)
-	{
-		$.fn.fontResizer = function(options)
-		{
-			//init config
-			var data = {};
-			//container of the text
-			data.readarea = this;
-			//increment steps
-			data.incsteps = 5;
-			//increment size
-			data.incsize = 2;
-			//element to set the font smaller
-			data.smallerbutton = null;
-			//element to set the font bigger
-			data.biggerbutton = null;
-			//array of elements that will change within the container
-			data.elements = ['div', 'p', 'li', 'blockquote', 'cite', 'dd', 'dt', 'h1','h2','h3', 'h4', 'h5', 'h6', 'span', 'small', 'td', 'th'];
-			//counter to steps
-			data.inc = 0;
-			//fontsize var
-			data.fontsize = 0;
-			//callback function on each font size action
-			data.callback = null;
-			//extending
-			var config = $.extend(data,options); 
-			//testing if the buttons are defined and send the error to console
-			if(config.smallerbutton == null || config.biggerbutton == null)
-			{
-				if(config.smallerbutton == null)
-				{
-					console.log('\'smallerbutton\' must be defined.')
-				}
-				if(config.biggerbutton == null)
-				{
-					console.log('\'biggerbutton\' must be defined.')
-				}
-			}
-			//if the buttons are defined
-			else if(config.smallerbutton != null && config.biggerbutton !=null)
-			{
-				//attaches the click event to buttons
-				config.smallerbutton.on('click', function(evt)
-					{
-						//cancels the default event # or link if exists
-						evt.preventDefault();
-						//sets the fontsize on each click passing the config object as parameter
-						smallerFontSize(config);
-					});
-				config.biggerbutton.on('click', function(evt)
-					{
-						//cancels the default event # or link if exists
-						evt.preventDefault();
-						//sets the fontsize on each click passing the config object as parameter
-						biggerFontSize(config);
-					});
-			}
-			//helper functions
-			//smaller fontsize
-			var smallerFontSize = function(data)
-			{
-				//counts the steps
-				if(data.inc>0)
-				{
-					data.inc--;
-					//decreases the fontsize using the incsize on data (config) object
-					data.fontsize = (-data.incsize);
-					//sets the font size
-					setFontSize(data);
-				}
-				
-			}
-			//bigger fontsize
-			var biggerFontSize = function(data)
-			{
-				//counts the steps
-				if(data.inc < data.incsteps)
-				{
-					data.inc++;
-					//increases the fontsize using the incsize on data (config) object
-					data.fontsize = (data.incsize);
-					//sets the font size
-					setFontSize(data);
-				}
-				
-			}
-			//changing fontsize on each element in array
-			var setFontSize = function(data)
-			{
-				//test if the text container have text outside elements like p, li, td, etc
-				if(data.readarea.find('p').length <=0 && data.readarea.find('div').length <=0 )
-				{
-					//if true, apply the resize to the parent element
-					var fs = Number(String(data.readarea.css('fontSize')).split("px")[0]);
-					data.readarea.css('fontSize', ((fs) + (data.fontsize)) + 'px');
-					if(data.callback!=null)
-					{
-						//if a callback was assigned, calls
-						data.callback();
-					}
-				}
-				//or, if the text is placed inside child elements of the text container
-				else
-				{
-					//loop trough the elements array
-					for(var $i = 0; $i < data.elements.length; $i++)
-					{
-						//for each element as textcontainer (only one is recommended)
-						data.readarea.each(function(){
-								//creates an instance for the text container
-								var ra = $(this);
-								//find each element in array within de text container
-								ra.find(data.elements[$i]).each(function(){
-										//creates an instance for that
-										var el = $(this);
-										//works the fontsize in numbers
-										var fs = Number(String(el.css('fontSize')).split("px")[0]);
-										//apply the new font size
-										el.css('fontSize', ((fs) + (data.fontsize)) + 'px');
-									})
-								//calls a callback functions, if exists
-								if(data.callback!=null)
-								{
-									data.callback();
-								}
-							});
-					}
-				}
-			}
-			//returning the current element, the text container
-			return this;
+var pluginName = "fontResizer",
+	defaults = {
+		steps: 5,
+		sizeIncrement: 2,
+		smallerButton: "",
+		biggerButton: "",
+		elements: ["p", "li", "blockquote", "cite", "dd", "dt", "h1", "h2", "h3", "h4", "h5", "h6", "span", "small", "td", "th"],
+		onIncrease: null,
+		onDecrease: null
+	};
+
+function FontResizer(element, options) {
+	this.container = $(element);
+	this.settings = $.extend({}, defaults, options);
+	this.init(options);
+}
+
+FontResizer.prototype = {
+	init: function() {
+		var self = this;
+		
+		self.step = Math.ceil(self.settings.steps / 2);
+		self.settings.elements = self.settings.elements.join(",");
+		self.settings.biggerButton.on("click."+pluginName, function(event) {self.increase(this, event);});
+		self.settings.smallerButton.on("click."+pluginName, function(event) {self.decrease(this, event);});
+	},
+	
+	increase: function(element, event) {
+		var self = this;
+		event.preventDefault();
+		
+		if (self.step + 1 > self.settings.steps) {
+			return false;
 		}
-	})(jQuery);
+		++self.step;
+		self.setFontSize(function(size) {return size + self.settings.sizeIncrement;});
+		self.trigger("onIncrease", element, event);
+	},
+	
+	decrease: function(element, event) {
+		var self = this;
+		event.preventDefault();
+		
+		if (self.step - 1 == 0) {
+			return false;
+		}
+		--self.step;
+		self.setFontSize(function(size) {return size - self.settings.sizeIncrement;});
+		self.trigger("onDecrease", element, event);
+	},
+	
+	getSizeSpecs: function(font_size) {
+		var result = /(-?\d+)(.+)/.exec(font_size);
+		return {
+			number: parseFloat(result[1]),
+			unit: result[2]
+		};
+	},
+	
+	setFontSize: function(getNewValue) {
+		var self = this;
+		
+		self.container.find(this.settings.elements).each(function() {
+			var element = $(this),
+				specs = self.getSizeSpecs(element.css("font-size"));
+			
+			element.css("font-size", getNewValue(specs.number) + specs.unit);
+		});
+	},
+	
+	trigger: function(callback, element, event) {
+		if (!this.settings[callback]) {
+			return;
+		}
+		this.settings[callback].call(
+			element, 
+			event,
+			this.step,
+			this.settings.steps,
+			{
+				smallerButton: this.settings.smallerButton,
+				biggerButton: this.settings.biggerButton
+			},
+			this.container
+		);
+	}
+};
 
+$.fn[pluginName] = function(options) {
+	var config = $.extend({}, defaults, options);
+	
+	config.smallerButton = $(config.smallerButton);
+	
+	if (config.smallerButton.length !== 1) {
+		console.error("smallerButton invalid");
+		return false;
+	}
+	
+	config.biggerButton = $(config.biggerButton);
+	
+	if (config.biggerButton.length !== 1) {
+		console.error("biggerButton invalid");
+		return false;
+	}
+	
+	return this.each(function () {
+		if (!$.data(this, "plugin_"+pluginName)) {
+			$.data(this, "plugin_"+pluginName, new FontResizer(this, config));
+		}
+	});
+};
+
+})(jQuery);

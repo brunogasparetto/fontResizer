50cc619478cf953f2f940c26f49d6cd1fb3f03bf
Bruno Gasparetto <bruno@ead.ucdb.br>
2014-08-20 09:43:43 -0400

    Código corrigido para validar com JSLint

--- jquery.fontResizer.js
+++ jquery.fontResizer.js
@@ -1,3 +1,5 @@
+/*global jQuery */
+
 /*! 
  * jquery.fontResizer - v 1.4
  * Copyright (c) 2014 Eder Lima - http://ederlima.com.br
@@ -20,118 +22,120 @@
  * The callbacks receive the event, current step, max steps, the buttons (smallerButton and biggerButton) and the container.
  * The context in the callbacks is the element trigger the font resize.
 */
-(function($) {
+(function ($, console) {
+    "use strict";
 
-var pluginName = "fontResizer",
-	defaults = {
-		steps: 5,
-		sizeIncrement: 2,
-		smallerButton: "",
-		biggerButton: "",
-		elements: ["p", "li", "blockquote", "cite", "dd", "dt", "h1", "h2", "h3", "h4", "h5", "h6", "span", "small", "td", "th"],
-		onIncrease: null,
-		onDecrease: null
-	};
+    var pluginName = "fontResizer",
+        defaults = {
+            steps: 5,
+            sizeIncrement: 2,
+            smallerButton: "",
+            biggerButton: "",
+            elements: ["p", "li", "blockquote", "cite", "dd", "dt", "h1", "h2", "h3", "h4", "h5", "h6", "span", "small", "td", "th"],
+            onIncrease: null,
+            onDecrease: null
+        };
 
-function FontResizer(element, options) {
-	this.container = $(element);
-	this.settings = $.extend({}, defaults, options);
-	this.init();
-}
+    function FontResizer(element, options) {
+        this.container = $(element);
+        this.settings = $.extend({}, defaults, options);
+        this.init();
+    }
 
-FontResizer.prototype = {
-	init: function() {
-		var self = this;
-		
-		self.step = Math.ceil(self.settings.steps / 2);
-		self.settings.elements = self.settings.elements.join(",");
-		self.settings.biggerButton.on("click."+pluginName, function(event) {self.increase(this, event);});
-		self.settings.smallerButton.on("click."+pluginName, function(event) {self.decrease(this, event);});
-	},
-	
-	increase: function(element, event) {
-		var self = this;
-		event.preventDefault();
-		
-		if (self.step + 1 > self.settings.steps) {
-			return false;
-		}
-		++self.step;
-		self.setFontSize(function(size) {return size + self.settings.sizeIncrement;});
-		self.trigger("onIncrease", element, event);
-	},
-	
-	decrease: function(element, event) {
-		var self = this;
-		event.preventDefault();
-		
-		if (self.step - 1 == 0) {
-			return false;
-		}
-		--self.step;
-		self.setFontSize(function(size) {return size - self.settings.sizeIncrement;});
-		self.trigger("onDecrease", element, event);
-	},
-	
-	setFontSize: function(getNewValue) {
-		var self = this;
-		
-		self.container.find(this.settings.elements).each(function() {
-			var element = $(this),
-				specs = self.getSizeSpecs(element.css("font-size"));
-			
-			element.css("font-size", getNewValue(specs.number) + specs.unit);
-		});
-	},
-	
-	getSizeSpecs: function(fontSize) {
-		var result = /(-?\d+)(.+)/.exec(fontSize);
-		return {
-			number: parseFloat(result[1]),
-			unit: result[2]
-		};
-	},
-	
-	trigger: function(callback, element, event) {
-		if (!this.settings[callback]) {
-			return;
-		}
-		this.settings[callback].call(
-			element, 
-			event,
-			this.step,
-			this.settings.steps,
-			{
-				smallerButton: this.settings.smallerButton,
-				biggerButton: this.settings.biggerButton
-			},
-			this.container
-		);
-	}
-};
+    FontResizer.prototype = {
+        init: function () {
+            var self = this;
 
-$.fn[pluginName] = function(options) {
-	var config = $.extend({}, defaults, options);
-	
-	config.smallerButton = $(config.smallerButton);
-	
-	if (config.smallerButton.length !== 1) {
-		console.error("smallerButton invalid");
-		return false;
-	}
-	
-	config.biggerButton = $(config.biggerButton);
-	
-	if (config.biggerButton.length !== 1) {
-		console.error("biggerButton invalid");
-		return false;
-	}
-	
-	return this.each(function () {
-		if (!$.data(this, "plugin_"+pluginName)) {
-			$.data(this, "plugin_"+pluginName, new FontResizer(this, config));
-		}
-	});
-};
+            self.step = Math.ceil(self.settings.steps / 2);
+            self.settings.elements = self.settings.elements.join(",");
+            self.settings.biggerButton.on("click." + pluginName, function (event) { self.increase(this, event); });
+            self.settings.smallerButton.on("click." + pluginName, function (event) { self.decrease(this, event); });
+        },
 
-})(jQuery);
+        increase: function (element, event) {
+            var self = this;
+            event.preventDefault();
+
+            if (self.step + 1 > self.settings.steps) {
+                return false;
+            }
+            self.step += 1;
+            self.setFontSize(function (size) { return size + self.settings.sizeIncrement; });
+            self.trigger("onIncrease", element, event);
+        },
+
+        decrease: function (element, event) {
+            var self = this;
+            event.preventDefault();
+
+            if (self.step - 1 === 0) {
+                return false;
+            }
+            self.step -= 1;
+            self.setFontSize(function (size) { return size - self.settings.sizeIncrement; });
+            self.trigger("onDecrease", element, event);
+        },
+
+        setFontSize: function (getNewValue) {
+            var self = this;
+
+            self.container.find(this.settings.elements).each(function () {
+                var element = $(this),
+                    specs = self.getSizeSpecs(element.css("font-size"));
+
+                element.css("font-size", getNewValue(specs.number) + specs.unit);
+            });
+        },
+
+        getSizeSpecs: function (fontSize) {
+            var result = /(-?\d+)(\D+)/.exec(fontSize);
+            
+            return {
+                number: parseFloat(result[1]),
+                unit: result[2]
+            };
+        },
+
+        trigger: function (callback, element, event) {
+            if (!this.settings[callback]) {
+                return;
+            }
+            this.settings[callback].call(
+                element,
+                event,
+                this.step,
+                this.settings.steps,
+                {
+                    smallerButton: this.settings.smallerButton,
+                    biggerButton: this.settings.biggerButton
+                },
+                this.container
+            );
+        }
+    };
+
+    $.fn[pluginName] = function (options) {
+        var config = $.extend({}, defaults, options);
+
+        config.smallerButton = $(config.smallerButton);
+
+        if (config.smallerButton.length !== 1) {
+            console.error("smallerButton invalid");
+            return false;
+        }
+
+        config.biggerButton = $(config.biggerButton);
+
+        if (config.biggerButton.length !== 1) {
+            console.error("biggerButton invalid");
+            return false;
+        }
+
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new FontResizer(this, config));
+            }
+        });
+    };
+
+}(jQuery, window.console));
